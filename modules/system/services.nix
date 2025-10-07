38343f7d9d25507cedf187fd75b0270717c82ca8
Sander Mirck <sandermirck@gmail.com>
2025-10-07 14:28:20 +0200

    Added Services = {     tailscale };

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -29,4 +29,7 @@
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     '';
   };
+
+  # Enable the Tailscale service
+  services.tailscale.enable = true;
 }
