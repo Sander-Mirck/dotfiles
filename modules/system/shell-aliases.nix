c33ccbb224bbf54b28022e34fad9c1f8c1906e06
Sander Mirck <mail@sandermirck.nl.eu.org>
2025-11-15 10:24:45 +0100

    Changed shell aliases from "nixos" to "laptop"

--- modules/system/shell-aliases.nix
+++ modules/system/shell-aliases.nix
@@ -3,15 +3,15 @@
 
 {
   environment.shellAliases = {
-    # NixOS rebuild commands
-    nrs = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
-    nrsu = "sudo nixos-rebuild switch --upgrade --flake /etc/nixos#nixos";
-    nrb = "sudo nixos-rebuild boot --flake /etc/nixos#nixos";
-    nrt = "sudo nixos-rebuild test --flake /etc/nixos#nixos";
+    # laptop rebuild commands
+    nrs = "sudo laptop-rebuild switch --flake /etc/laptop#laptop";
+    nrsu = "sudo laptop-rebuild switch --upgrade --flake /etc/laptop#laptop";
+    nrb = "sudo laptop-rebuild boot --flake /etc/laptop#laptop";
+    nrt = "sudo laptop-rebuild test --flake /etc/laptop#laptop";
 
     # Nix garbage collection
     nrgc = "sudo nix-collect-garbage -d";
-    nix-update = "sudo nixos-rebuild switch --flake /etc/nixos#nixos --upgrade";
+    nix-update = "sudo laptop-rebuild switch --flake /etc/laptop#laptop --upgrade";
 
     # Utility aliases
     ll = "ls -la";
