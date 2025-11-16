75728b35ad97898c7aacc54b26d3b665733f507a
Sander Mirck <mail@sandermirck.nl.eu.org>
2025-11-16 11:57:05 +0100

    Fix dev.nix

--- lib/flake-modules/dev.nix
+++ lib/flake-modules/dev.nix
@@ -1,26 +1,31 @@
 # /lib/flake-modules/dev.nix
 # This flake module defines developer-centric outputs like formatters and shells.
-{inputs, ...}: {
-  perSystem = {
-    config,
-    self',
-    pkgs,
-    ...
-  }: {
-    # Default formatter for `nix fmt`.
-    formatter = pkgs.alejandra;
+{ inputs, ... }:
+{
+  perSystem =
+    {
+      config,
+      self',
+      pkgs,
+      ...
+    }:
+    {
+      # Default formatter for `nix fmt`.
+      formatter = pkgs.alejandra;
 
-    # Linter to detect dead code.
-    packages.deadnix = inputs.deadnix.packages.default;
+      # Linter to detect dead code.
+      # We must specify the system architecture to get the correct package.
+      packages.deadnix = inputs.deadnix.packages.${pkgs.system}.default;
 
-    # Development shell with essential tools.
-    devShells.default = pkgs.mkShell {
-      packages = [
-        pkgs.git
-        pkgs.nil
-        pkgs.alejandra
-        self'.packages.deadnix
-      ];
+      # Development shell with essential tools.
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.git
+          pkgs.nil
+          pkgs.alejandra
+          # self' refers to the outputs defined in this perSystem block.
+          self'.packages.deadnix
+        ];
+      };
     };
-  };
 }
